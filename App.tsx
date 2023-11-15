15a48fd4cea2012be032c66e461d2609e1047592
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-11-15 09:58:21 +0330

    76 - Creating User Posts using Flatlist English-2

--- App.tsx
+++ App.tsx
@@ -1,171 +1,179 @@
-import React, { useState } from "react";
-import {SafeAreaView,ScrollView,View,Pressable,Text, FlatList } from "react-native";
-import Title from "./components/Title/Title";
-import UserStory from "./components/UserStory/UserStory";
-import UserPost from "./components/UserPost/UserPost";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import React, {useState} from 'react';
+import {
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  FlatList,
+} from 'react-native';
+
+//Custom Components
+import Title from './components/Title/Title';
+import UserStory from './components/UserStory/UserStory';
+import UserPost from './components/UserPost/UserPost';
+//FontAwesome Icons
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+
+//Style Imports
 import style from './assets/styles/main';
-const App=()=> {
-    //All of the items in our stories
+
+const App = () => {
+  //All of the items in our stories
   const data = [
     {
-      firstName:'Ali',
-       id:1,
-    },
-    {  
-       firstName:'Amin',
-       id:2,
-     },
-    {
-      firstName:'Joseph',
-       id:3,
-    },
-    {  
-       firstName:'Angel',
-       id:4,
-     },
-     {
-      firstName:'White',
-      id:5,
-     },
-     {  
-        firstName:'Oliver',
-        id:6,
+      firstName: 'Joseph',
+      id: 1,
     },
     {
-      firstName:'Nata',
-       id:7,
+      firstName: 'Angel',
+      id: 2,
     },
-    {  
-      firstName:'Adam',
-       id:8,
+    {
+      firstName: 'White',
+      id: 3,
     },
+    {
+      firstName: 'Olivier',
+      id: 4,
+    },
+    {
+      firstName: 'Nata',
+      id: 5,
+    },
+    {
+      firstName: 'Adam',
+      id: 6,
+    },
+    {
+      firstName: 'Sean',
+      id: 7,
+    },
+    {
+      firstName: 'Nicolas',
+      id: 8,
+    },
+    {
+      firstName: 'Frederic',
+      id: 9,
+    },
+  ];
+  const posts = [
+    {
+      firstName: 'Allison',
+      lastName: 'Becker',
+      location: 'Sukabumi, Jawa Barat',
+      likes: 1201,
+      comments: 24,
+      bookmarks: 55,
+      id: 1,
+    },
+    {
+      firstName: 'Jennifer',
+      lastName: 'Wilkson',
+      location: 'Pondok Leungsir, Jawa Barat',
+      likes: 570,
+      comments: 12,
+      bookmarks: 60,
+      id: 2,
+    },
+    {
+      firstName: 'Adam',
+      lastName: 'Spera',
+      location: 'Boston, Massachusetts',
+      likes: 100,
+      comments: 8,
+      bookmarks: 7,
+      id: 3,
+    },
+    {
+      firstName: 'Nata',
+      lastName: 'Vacheishvili',
+      location: 'New York, New York',
+      likes: 300,
+      comments: 18,
+      bookmarks: 17,
+      id: 4,
+    },
+    {
+      firstName: 'Nicolas',
+      lastName: 'Namoradze',
+      location: 'Berlin, Germany',
+      likes: 1240,
+      comments: 56,
+      bookmarks: 20,
+      id: 5,
+    },
+  ];
+  // Define page size constant for the number of items to be displayed per page
+  const pageSize = 4;
+  const pageSizePosts = 2;
 
-    {
-      firstName:'Ali',
-       id:9,
-    },
-    {  
-       firstName:'Amin',
-       id:10,
-     },
-    {
-      firstName:'Joseph',
-       id:11,
-    },
-    {  
-       firstName:'Angel',
-       id:12,
-     },
-     {
-      firstName:'White',
-      id:13,
-     },
-     {  
-        firstName:'Oliver',
-        id:14,
-    },
-    {
-      firstName:'Nata',
-       id:15,
-    },
-    {
-      firstName:'White',
-      id:16,
-     },
-     {  
-        firstName:'Oliver',
-        id:17,
-    },
-    {
-      firstName:'Nata',
-       id:18,
-    },
-  
-];
-const posts = [
-  {
-    firstName: 'Allison',
-    lastName: 'Becker',
-    location: 'Sukabumi, Jawa Barat',
-    likes: 1201,
-    comments: 24,
-    bookmarks: 55,
-    id: 1,
-  },
-  {
-    firstName: 'Jennifer',
-    lastName: 'Wilkson',
-    location: 'Pondok Leungsir, Jawa Barat',
-    likes: 570,
-    comments: 12,
-    bookmarks: 60,
-    id: 2,
-  },
-  {
-    firstName: 'Adam',
-    lastName: 'Spera',
-    location: 'Boston, Massachusetts',
-    likes: 100,
-    comments: 8,
-    bookmarks: 7,
-    id: 3,
-  },
-  {
-    firstName: 'Nata',
-    lastName: 'Vacheishvili',
-    location: 'New York, New York',
-    likes: 300,
-    comments: 18,
-    bookmarks: 17,
-    id: 4,
-  },
-  {
-    firstName: 'Nicolas',
-    lastName: 'Namoradze',
-    location: 'Berlin, Germany',
-    likes: 1240,
-    comments: 56,
-    bookmarks: 20,
-    id: 5,
-  },
-];
-const pageSize=4;
-const pageSizePosts = 2;
-const [pageNumber, setPageNumber]= useState(1);
-const[isLoading,setIsLoading]= useState(false);
-const[isLoadingPosts,setIsLoadingPosts]= useState(false);
-const[renderedData, setRenderedData]= useState(data.slice(0,pageSize));
-const[renderedDataPosts, setRenderedDataPosts]= useState(posts.slice(0,pageSize));
-const pagination = (data, pageNumber, pageSize, posts = false) => {
-  let startIndex = (pageNumber - 1) * pageSize;
-  //don't return the information that does not exist inside the data array
-  if (startIndex >= data.length) {
-    return [];
-  }
-  if (!posts) {
-    //set the page number, to the page number that we wanted to fetch so that we have information
-    //about which page was the one that was last fetched
-    setPageNumber(pageNumber);
-  } else {
-    setPostPageNumber(pageNumber);
-  }
-  return data.slice(startIndex, startIndex + pageSize);
-};
+  // Define state variable for the current page number
+  // so that we know how many pages we have fetched already
+  const [pageNumber, setPageNumber] = useState(1);
+  const [postPageNumber, setPostPageNumber] = useState(1);
 
-return(
-  <SafeAreaView>
-   
+  // Define state variable for the loading status of the flatlist,
+  // will be used when we'll be fetching data on scroll until we complete the fetch
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingPosts, setIsLoadingPosts] = useState(false);
+
+  // Define state variable for the data to be rendered in the user story container
+  const [renderedData, setRenderedData] = useState(data.slice(0, pageSize));
+
+  // Define state variable for the data to be rendered in the user posts container
+  //@TODO This should be pageSizePosts, I forgot to mention it in this particular lesson
+  //we'll fix that in the upcoming lessons
+  const [renderedDataPosts, setRenderedDataPosts] = useState(
+    posts.slice(0, pageSize),
+  );
+
+  /**
+   * function that returns the data for the page to be fetched
+   * @param data - all the data
+   * @param pageNumber - page number to fetch
+   * @param pageSize - number of items to fetch for the page
+   * @param posts - boolean to determine if we're using pagination for posts or user stories
+   */
+  const pagination = (data, pageNumber, pageSize, posts = false) => {
+    let startIndex = (pageNumber - 1) * pageSize;
+    //don't return the information that does not exist inside the data array
+    if (startIndex >= data.length) {
+      return [];
+    }
+    if (!posts) {
+      //set the page number, to the page number that we wanted to fetch so that we have information
+      //about which page was the one that was last fetched
+      setPageNumber(pageNumber);
+    } else {
+      setPostPageNumber(pageNumber);
+    }
+    return data.slice(startIndex, startIndex + pageSize);
+  };
+
+  return (
+    // Use the SafeAreaView component to ensure content is displayed within the safe area boundaries of the device
+    <SafeAreaView>
+      {/* Use ScrollView to allow users to scroll through content */}
+      <ScrollView>
+        {/* Use View to create a container for title and icon */}
         <View style={style.header}>
-           <Title title={"Let's Explore"} />
-             <Pressable style={style.messageIcon}> 
-               <FontAwesomeIcon icon={faEnvelope}  color={'#CACDDE'} size={20}/>
-                <View style={style.messageNumberContainer}>
-                <Text style={style.messageNumber}>2</Text>
-              </View>
+          {/* Use custom Title component to display the title */}
+          <Title title={"Let's Explore"} />
+          {/* Use Pressable to create a clickable component */}
+          <Pressable style={style.messageIcon}>
+            {/* Use FontAwesomeIcon component to display an icon from FontAwesome icon set */}
+            <FontAwesomeIcon icon={faEnvelope} color={'#CACDDE'} size={20} />
+            {/* Use View to create a container for message number */}
+            <View style={style.messageNumberContainer}>
+              {/* Use Text to display the number of messages */}
+              <Text style={style.messageNumber}>2</Text>
+            </View>
           </Pressable>
-      </View>
+        </View>
+        {/* Use View to create a container for the user stories */}
       <View style={style.userStoryContainer}>
               <FlatList 
                onEndReachedThreshold={0.5}
@@ -214,7 +222,9 @@ return(
 
      
     
-  </SafeAreaView>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
 export default App;
