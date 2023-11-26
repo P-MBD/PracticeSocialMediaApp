23afb88dcfe216a1cb23002a92cf58004c13acae
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-11-26 11:45:26 +0330

    92 - Setting up Navigation System

---
+++ components/UserStory/style.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+const style= StyleSheet.create({
+    storyContainer:{
+        paddingHorizontal:28,
+        marginRight:12,    
+        height:100,   
+    },
+    name:{
+        textAlign:'center',
+        fontFamily:'Inter',
+        fontWeight:'500',
+        fontSize:14,
+        lineHeight:17,
+        marginTop:8,
+    },
+    userImageContainer:{
+        borderWidth:1,
+        borderColor:'#F35BAC',
+        padding:3,
+        borderRadius:50,
+    }
+});
+export default style;
