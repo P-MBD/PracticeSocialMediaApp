ad26266761cbfaa9f1e8ba1736cd8783f84058c9
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-12-10 11:19:24 +0330

    98 - How to Style Tab Labels

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -4,6 +4,7 @@ import Home from "../Screens/Home/Home";
 import Profile from "../Screens/Profile/Profile";
 import { Routes } from "./Routes";
 import {View, Text} from 'react-native';
+import ProfileTabTitle from "../components/ProfileTabTitle/ProfileTabTitle";
 const Stack = createStackNavigator();
 const ProfileTabs = createMaterialTopTabNavigator();
 const Tab1= () => {
@@ -27,15 +28,49 @@ const Tab3 = () => {
         </View>
     );
 }
-export const ProfileTabNavigation= () => {
-    return(
-        <ProfileTabs.Navigator>
-            <ProfileTabs.Screen name={"Tab1"} component={Tab1} />
-            <ProfileTabs.Screen name={"Tab2"} component={Tab2} />
-            <ProfileTabs.Screen name={"Tab3"} component={Tab3} />
-        </ProfileTabs.Navigator>
+export const ProfileTabNavigation = () => {
+    return (
+      <ProfileTabs.Navigator
+        screenOptions={{
+          tabBarIndicatorStyle: {
+            backgroundColor: 'transparent',
+          },
+          tabBarStyle: {
+            elevation: 0,
+            zIndex: 0,
+          },
+        }}>
+        <ProfileTabs.Screen
+          name={'Tab1'}
+          component={Tab1}
+          options={{
+            tabBarLabel: ({focused}) => {
+              return <ProfileTabTitle title={'Photos'} isFocused={focused} />;
+            },
+          }}
+        />
+        <ProfileTabs.Screen
+          name={'Tab2'}
+          component={Tab2}
+          options={{
+            tabBarLabel: ({focused}) => {
+              return <ProfileTabTitle title={'Videos'} isFocused={focused} />;
+            },
+          }}
+        />
+        <ProfileTabs.Screen
+          name={'Tab3'}
+          component={Tab3}
+          options={{
+            tabBarLabel: ({focused}) => {
+              return <ProfileTabTitle title={'Saved'} isFocused={focused} />;
+            },
+          }}
+        />
+      </ProfileTabs.Navigator>
     );
-}
+  };
+  
 
 const MainNavigation = () => {
     return(
