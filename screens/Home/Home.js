be15b46ba3865f757d9733536e5573a1a0d9e00c
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-12-20 11:44:16 +0330

    111 - Installing Font Awesome Icons English

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 import Header from '../../components/Header/Header';
-
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
 import Button from '../../components/Button/Button';
 import Tab from '../../components/Tab/Tab';
 import { horizontalScale } from '../../assets/styles/scaling';
 import Badge from '../../components/Badge/Badge';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       <Header title={'Azzahri A.'} type={1} />
       <Tab title={'Highlight'}/>
       <Badge title={'Environment'}/>
+      <FontAwesomeIcon icon={faSearch} />
     </SafeAreaView>
   );
 };
