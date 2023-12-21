3ca7176b988a60bbdc0353c39f74673ce296b143
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-12-21 10:21:33 +0330

    112 - Creating Search Input Component English

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -9,14 +9,12 @@ import { horizontalScale } from '../../assets/styles/scaling';
 import Badge from '../../components/Badge/Badge';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import Search from '../../components/Search/Search';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <Header title={'Azzahri A.'} type={1} />
-      <Tab title={'Highlight'}/>
-      <Badge title={'Environment'}/>
-      <FontAwesomeIcon icon={faSearch} />
+      <Search onSearch={(value)=>{console.log(value)}} />
     </SafeAreaView>
   );
 };
